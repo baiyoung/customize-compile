1b5c7023ad8d4a09d5e99d5bd4491fc54d4bf308
白杨 <629730936@qq.com>
2021-10-22 17:11:06 +0800

    update: luci-theme-argon

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # github package
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git  package/lean
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate

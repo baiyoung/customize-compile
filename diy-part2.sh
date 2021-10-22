0ba807e7955cf32ff31656eba9c02c556b3820a3
白杨 <629730936@qq.com>
2021-10-23 03:29:48 +0800

    update: 经典版主题

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,8 @@
 mkdir package/community
 pushd package/community
 rm -rf ../lean/luci-theme-argon
-git clone --depth 1  https://github.com/jerrykuku/luci-theme-argon
+# git clone --depth 1  https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 popd
 pwd
 pushd package/network/services

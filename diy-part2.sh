9b586946e7832e5d5f60fd6ae03fdbd7a1e99a92
白杨 <629730936@qq.com>
2021-10-23 18:12:06 +0800

    update: 稳定版本

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,4 +35,4 @@ popd
 # git clone https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate

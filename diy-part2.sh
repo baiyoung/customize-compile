e0d53422ecf97beb49dfabe46fdc8a51d6bd245e
bywebol <629730936@qq.com>
2022-04-04 20:54:10 +0800

    更新配置

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,11 +11,11 @@
 #
 # github package
 mkdir package/community
-pushd package/community
-rm -rf ../lean/luci-theme-argon
-# git clone --depth 1  https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-popd
+# pushd package/community
+# rm -rf ../lean/luci-theme-argon
+# # git clone --depth 1  https://github.com/jerrykuku/luci-theme-argon
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+# popd
 pwd
 
 # Add cpufreq

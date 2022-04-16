5eff159858d7b4eff07430d33b99b89b240a9f70
bywebol <629730936@qq.com>
2022-04-16 17:10:39 +0800

    更新配置

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ svn co https://github.com/openwrt/packages/trunk/libs/libssh
 popd
 
 rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
